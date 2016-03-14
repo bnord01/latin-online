f40cd27efbfa053027892fa58f2c9c5195b5dbdd
bnord <bnord01@gmail.com>
2016-03-14 17:34:59 +0100

    update on delete

--- public/js/application.js
+++ public/js/application.js
@@ -70,7 +70,8 @@ angular.module("latin-o", ['ui.bootstrap'])
     $scope.remove = function(latin) {
         delete $scope.dictionary[latin];
         $scope.keys = Object.keys($scope.dictionary)
-        $http.post("remove",{latin:latin})
+        $http.post("remove",{latin:latin})        
+        updateRandomLatin();
     }
   });
 
