625b7319abe2efdaa1827e716858d6b0a6e1eadd
bnord <bnord01@gmail.com>
2016-03-17 11:50:40 +0100

    show error on failed add, focus, hide if nothing to train

--- public/js/application.js
+++ public/js/application.js
@@ -1,116 +1,154 @@
 'use strict';
 const ENABLE_REMOVE = true;
 
-angular.module("latin-o", ['ui.bootstrap'])
-  .controller("LatinOController", function($scope, $http, $filter, $uibModal) {
-    $scope.dictionary;
-    $scope.learnset;
-    $scope.keys;
-    $scope.current_latin;
-    $scope.current_german = "";
-    $scope.correct = true;
-    $scope.mistakes=[];
-    $scope.last_latin
-    $scope.last_expected
-    $scope.last_input
-    $scope.enable_remove = ENABLE_REMOVE;
+const app = angular.module("latin-o", ['ui.bootstrap'])
 
-    $http.get("dictionary").success(function(data) {
-      $scope.dictionary = data;
-      $scope.keys = Object.keys($scope.dictionary);
-    });
+app.controller("LatinOController", function($scope, $http, $filter, $uibModal, focus) {
+        $scope.dictionary;
+        $scope.learnset;
+        $scope.keys;
+        $scope.current_latin;
+        $scope.current_german = "";
+        $scope.correct = true;
+        $scope.mistakes = [];
+        $scope.last_latin
+        $scope.last_expected
+        $scope.last_input
+        $scope.enable_remove = ENABLE_REMOVE;
+        $scope.dict_error
 
-    $http.get("learnset").success(function(data) {
-      $scope.learnset = data;
-      updateRandomLatin();
-    });
-
-
-    // Open info modal
-    $scope.openInfo = function() {
-      $uibModal.open({
-        animation: true,
-        templateUrl: 'InfoModal.html',
-        controller: 'InfoModalInstanceCtrl',
-        size: "lg"
-      });
-    };
-
-    $scope.numEntries = function () {
-        return $scope.keys.length;
-    }
-
-    $scope.addTranslation = function () {
-        if ($scope.latin && $scope.german) {
-            $scope.dictionary[$scope.latin] = $scope.german.split(",").map(x => x.trim());
+        $http.get("dictionary").success(function(data) {
+            $scope.dictionary = data;
             $scope.keys = Object.keys($scope.dictionary);
-            $http.post("translation",{latin:$scope.latin,german:$scope.german});
-            $scope.latin=""
-            $scope.german=""
-        }
-        updateRandomLatin();
-    }
+        });
 
-    function updateRandomLatin() {
-        let learnset = $scope.learnset;
-        if(learnset.length>0) {
-            $scope.current_latin = learnset[Math.floor(Math.random() * learnset.length)];
-            $scope.current_german = "";
-        }
-    }
+        $http.get("learnset").success(function(data) {
+            $scope.learnset = data;
+            updateRandomLatin();
+        });
 
-    $scope.check = function() {
-        let input = $scope.current_german.split(",").map(x=>x.trim());
-        let latin = $scope.current_latin
-        let expected = $scope.dictionary[latin];
-        let correct = false
-        let mistakes = [];
-        for(let e of expected) {
-            if(!input.includes(e)){
-                mistakes.push("Eingabe enthält nicht: " + e)
+
+        // Open info modal
+        $scope.openInfo = function() {
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'InfoModal.html',
+                controller: 'InfoModalInstanceCtrl',
+                size: "lg"
+            });
+        };
+
+        $scope.numEntries = function() {
+            return $scope.keys.length;
+        }
+
+        $scope.addTranslation = function() {
+            let latin = $scope.latin
+            let german = $scope.german
+            if (latin && german) {
+                $http.post("translation", {
+                    latin: latin,
+                    german: german
+                }).then(res => {
+                    $scope.dictionary[latin] = german.split(",").map(x => x.trim());
+                    $scope.keys = Object.keys($scope.dictionary);
+                    $scope.learnset.push(latin)
+                    $scope.latin = ""
+                    $scope.german = ""
+                    $scope.dict_conflict = false
+                    focus('focusLatin')
+                    updateRandomLatin();
+                }).catch(err => {
+                    $scope.dict_conflict = true
+                });
+
+            }
+        }
+
+        function updateRandomLatin() {
+            let learnset = $scope.learnset;
+            if (learnset.length > 0) {
+                $scope.current_latin = learnset[Math.floor(Math.random() * learnset.length)];
+                $scope.current_german = "";
+            }
+        }
+
+        $scope.check = function() {
+            let input = $scope.current_german.split(",").map(x => x.trim());
+            let latin = $scope.current_latin
+            let expected = $scope.dictionary[latin];
+            let correct = false
+            let mistakes = [];
+            for (let e of expected) {
+                if (!input.includes(e)) {
+                    mistakes.push("Eingabe enthält nicht: " + e)
+                } else {
+                    correct = true;
+                }
+            }
+            for (let i of input) {
+                if (!expected.includes(i)) {
+                    mistakes.push("Wörterbuch enthält nicht: " + i)
+                    correct = false;
+                }
+            }
+            $scope.correct = correct;
+            $scope.mistakes = mistakes;
+            $scope.last_latin = $scope.current_latin
+            $scope.last_input = $scope.current_german
+            $scope.last_expected = expected.join(', ')
+
+            if (correct) {
+                let idx = $scope.learnset.indexOf(latin)
+                $scope.learnset.splice(idx, 1)
+                $http.post("correct", {
+                    phrase: latin
+                })
             } else {
-                correct = true;
+                $http.post("incorrect", {
+                    phrase: latin
+                })
             }
+            updateRandomLatin();
         }
-        for(let i of input) {
-            if(!expected.includes(i)){
-                mistakes.push("Wörterbuch enthält nicht: " + i)
-                correct = false;
-            }
-        }
-        $scope.correct = correct;
-        $scope.mistakes = mistakes;
-        $scope.last_latin = $scope.current_latin
-        $scope.last_input = $scope.current_german
-        $scope.last_expected = expected.join(', ')
 
-        if(correct){
-            let idx = $scope.learnset.indexOf(latin)
-            $scope.learnset.splice(idx,1)
-            $http.post("correct",{phrase:latin})
-        } else {
-            $http.post("incorrect",{phrase:latin})
-        }
-        updateRandomLatin();
-    }
-
-    if(ENABLE_REMOVE) {
-        $scope.remove = function(latin) {
-            let r = confirm("Eintrag "+ latin+ " wirklich löschen?")
-            if(r) {
-                delete $scope.dictionary[latin];
-                $scope.keys = Object.keys($scope.dictionary)
-                $http.post("remove",{latin:latin})
-                updateRandomLatin();
+        if (ENABLE_REMOVE) {
+            $scope.remove = function(latin) {
+                let r = confirm("Eintrag " + latin + " wirklich löschen?")
+                if (r) {
+                    delete $scope.dictionary[latin];
+                    $scope.keys = Object.keys($scope.dictionary)
+                    $http.post("remove", {
+                        latin: latin
+                    })
+                    updateRandomLatin();
+                }
             }
         }
-    }
-  });
+    });
+
+app.directive('focusOn', function() {
+    return function(scope, elem, attr) {
+        scope.$on('focusOn', function(e, name) {
+            if (name === attr.focusOn) {
+                elem[0].focus();
+            }
+        });
+    };
+});
+
+app.factory('focus', function ($rootScope, $timeout) {
+  return function(name) {
+    $timeout(function (){
+      $rootScope.$broadcast('focusOn', name);
+    });
+  }
+});
 
 
 // Controller for the info modal.
-angular.module('latin-o').controller('InfoModalInstanceCtrl', function($scope, $uibModalInstance) {
-  $scope.close = function() {
-    $uibModalInstance.dismiss('close');
-  };
+app.controller('InfoModalInstanceCtrl', function($scope, $uibModalInstance) {
+    $scope.close = function() {
+        $uibModalInstance.dismiss('close');
+    };
 });
